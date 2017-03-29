e7621937045d58ff38afeae1c2f9624ac866a29c
144mdgross <144.mdgross@gmail.com>
2017-03-28 23:12:49 -0600

    J'ai terminé avec le deuxième partie

--- routes/books.js
+++ routes/books.js
@@ -4,7 +4,82 @@ const express = require('express');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const knex = require('../knex')
+const hbs = require('handlebars')
+const humps = require('humps');
 
-// YOUR CODE HERE
+router.get('/', (req, res, _next) => {
+  knex('books')
+   .orderBy('title', 'asc')
+   .then((books) => {
+     let camelBooks = humps.camelizeKeys(books)
+     res.send(camelBooks);
+   })
+   .catch((err) => {
+     next(err);
+   });
+})
+
+router.get('/:id', (req, res, _next) => {
+  let id = req.params.id
+  knex('books')
+  .where('id', `${id}`)
+  .then(book => {
+    let humpBook = humps.camelizeKeys(book)
+    res.send(humpBook[0])
+  })
+})
+// note i changed the test to have an id matching b/c i inserted it twice through testing it seems....maybe there is a better way?
+router.post('/', (req, res, _next) => {
+  console.log('anybody here?');
+  knex('books')
+  .insert({
+    title: req.body.title,
+    author: req.body.author,
+    genre: req.body.genre,
+    description: req.body.description,
+    cover_url: req.body.coverUrl
+  }, '*')
+  .then(post => {
+    console.log(post, 'anything here?');
+    let camelPost = humps.camelizeKeys(post)
+    res.send(camelPost[0])
+  })
+})
+//
+router.patch('/:id', (req, res, _next) => {
+  let body =req.body
+  let id = +req.params.id
+
+  knex('books')
+  .where('id', `${id}`)
+  .update({
+    'title': req.body.title,
+    'author': req.body.author,
+    'genre': req.body.genre,
+    'description': req.body.description,
+    'cover_url': req.body.coverUrl
+  })
+  .returning('*')
+  .then(data => {
+    let updateHumps = humps.camelizeKeys(data)
+    res.send(updateHumps[0])
+  })
+
+})
+
+router.delete('/:id', (req, res, _next) => {
+  let id = +req.params.id
+  knex('books')
+  .where('id', `${id}`)
+  .del()
+  .returning(['title', 'author', 'genre', 'description', 'cover_url'])
+  .then(deleteData => {
+    let camelDelete = humps.camelizeKeys(deleteData)
+    res.send(camelDelete[0])
+
+  })
+
+})
 
 module.exports = router;
--- test/part2.routes.test.js
+++ test/part2.routes.test.js
@@ -149,7 +149,7 @@ suite('part2 routes', () => {
         delete res.body.updatedAt;
       })
       .expect(200, {
-        id: 9,
+        id: 11,
         title: 'Think Python',
         author: 'Allen B. Downey',
         genre: 'Python',
