e60a1432284d15a4c2251a2c832132ec72913611
144mdgross <144.mdgross@gmail.com>
2017-04-02 16:21:27 -0600

    all tests in part 5 passing. Work onsetval to be continued.

--- routes/favorites.js
+++ routes/favorites.js
@@ -10,7 +10,15 @@ const humps = require('humps')
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+function loggedIn(req, res, next) {
+  if(req.cookies.token) {
+    next()
+  } else {
+    return next(Boom.unauthorized('Unauthorized'))
+  }
+}
+
+router.get('/', loggedIn, (req, res, next) => {
   knex('favorites')
    .join('books', 'books.id', 'book_id')
    .then((books) => {
@@ -22,8 +30,7 @@ router.get('/', (req, res, next) => {
    });
 })
 
-router.get('/check', (req, res, next) => {
-  // this is where I stopped. Set this up to taken an id.
+router.get('/check', loggedIn, (req, res, next) => {
 let bookId = req.query.bookId
   knex('favorites')
    .join('books', 'books.id', 'book_id')
@@ -42,16 +49,43 @@ let bookId = req.query.bookId
    .catch((err) => {
      next(err);
    });
-
-  // check out express docs for handling this route
 })
 
-router.post('/', (req, res, next) => {
-  res.end()
-})
+router.post('/', loggedIn, (req, res, next) => {
+  const token = req.cookies
+  const favorite = req.body.bookId
 
-router.delete('/', (req, res, next) => {
-  res.end()
+  knex('favorites')
+  .insert([{
+    book_id: `${req.body.bookId}`,
+    user_id: 1,
+    // user_id: `${}`, // decrpyt jwt token and get user id
+   }])
+   .returning('*')
+   .then(newFav => {
+
+     let camelFav = humps.camelizeKeys(newFav)
+     res.status(200)
+       .set('Content-Type', 'application/json')
+       .send(camelFav[0])
+   })
+   .then((sql) => {
+     console.log('setval', sql);
+     return knex.raw("SELECT setval('favorites_id_seq', (SELECT MAX(id) FROM favorites));")
+})
+// setval not working. come back later. do manually for now.
+})
+router.delete('/', loggedIn, (req, res, next) => {
+  knex('favorites')
+  .where('book_id', `${req.body.bookId}`)
+  .del()
+  .returning('*')
+  .then(gone => {
+    console.log('GONE', gone);
+    let camelGone = humps.camelizeKeys(gone)
+
+    res.send({bookId: `${camelGone[0].bookId}`, userId: `${camelGone[0].userId}`})
+  })
 })
 
 module.exports = router;
