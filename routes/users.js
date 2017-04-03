2417ff7375de78c6f14198c80623413e013d4d4c
144mdgross <144.mdgross@gmail.com>
2017-04-02 19:08:03 -0600

    finished routes/users.js bonus

--- routes/users.js
+++ routes/users.js
@@ -8,42 +8,44 @@ let bcrypt = require('bcrypt');
 const saltRounds = 10;
 const myPlaintextPassword = 's0/\/\P4$$w0rD';
 const someOtherPlaintextPassword = 'not_bacon';
+const bodyParser = require('body-parser')
+const boom = require('boom')
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
-  // let hasEmail = false
-  // let emailDb = false
-  // let email = req.body.email
-  // (email === '') ? hasEmail = false : hasEmail = true
-  // let password = req.body.password
-  // let emailMatch = false
-  // knex('users')
-  // .where(`${email}`)
-  // .then(result => {
-  //   !result ? emailDb = false : emailDb = true
-  // })
+  let hasEmail = false
+  let emailDb = false
+  let email = req.body.email
 
-  // found example using sync...maybe not the best
-  // if (hasEmail && password.length > 8 && !emailMatch && !emailDb ) {
-  const salt = bcrypt.genSaltSync()
-  const hash = bcrypt.hashSync(req.body.password, salt)
-  knex('users')
-    .insert([{
-      first_name: req.body.firstName,
-      last_name: req.body.lastName,
-      email: req.body.email,
-      hashed_password: hash
-    }])
-    .returning(['id', 'first_name', 'last_name', 'email'])
-    .then(newUser => {
-      let camelUser = humps.camelizeKeys(newUser)
+
+  if (!req.body.password || req.body.password.length < 8) {
+    return next(boom.create(400, 'Password must be at least 8 characters long'))
+  } else {
+    const salt = bcrypt.genSaltSync()
+    const hash = bcrypt.hashSync(req.body.password, salt)
+
+    knex('users')
+      .insert([{
+        first_name: req.body.firstName,
+        last_name: req.body.lastName,
+        email: req.body.email,
+        hashed_password: hash
+      }])
+      .returning(['id', 'first_name', 'last_name', 'email'])
+      .then(newUser => {
+        let camelUser = humps.camelizeKeys(newUser)
         res.send(camelUser[0])
-    })
-  // } else if (!hasEmail) {
-  //   res.send('Email must not be blank')
-  // }
+      })
+      .catch(err => {
+        if (err.message.includes('unique')) {
+          return next(boom.create(400, 'Email already exists'))
+        } else {
+          return next(boom.create(400, 'Email must not be blank'))
+        }
+      })
+    }
 })
 
 module.exports = router;
